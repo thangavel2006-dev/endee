fba6fc8b2114cbfe2aa0bfb4f46f25cd83cbb9cc
hemant-endee <hemant@endee.io>
2026-04-06 19:15:15 +0530

    frontend version change (#165)

--- install.sh
+++ install.sh
@@ -197,7 +197,7 @@ distro_factory() {
 # ****************************************
 
 add_frontend() {
-    VERSION="v1.4.0"
+    VERSION="v1.6.0"
     log "Pulling frontend version ${VERSION}"
     mkdir -p $script_dir/frontend
     cd $script_dir/frontend
